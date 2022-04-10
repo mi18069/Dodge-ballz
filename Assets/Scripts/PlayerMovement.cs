a3be4fcc359db18e12687bf8f7a4e3cb9b03116d
j.markovic <markovicjovan26@gmail.com>
2022-04-10 18:41:27 +0200

    feature: Add propeller rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField, Range(1,5)] private float speed = 0.5f;
+    [SerializeField, Range(1,5)] private float speed = 1.5f;
     private float horizontal;
     private float vertical;
     // Start is called before the first frame update
