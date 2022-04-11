a90bbd6a7c733432e288b68e62a1295e60f28f04
j.markovic <markovicjovan26@gmail.com>
2022-04-11 12:23:37 +0200

    feature: Add flying
    Player can fly up to 1 second

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField, Range(1,5)] private float speed = 1.5f;
     private float horizontal;
     private float vertical;
+    private double maxFlyingTime = 1.0;
+    private double flyingTime = 1.0;
+    private bool canFly = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, 0f, 1.0f), transform.position.z); // FIX THIS
+        // Uncomment in order to debug
+        // Debug.Log(flyingTime);
         getInput();
         
         Move();
@@ -26,6 +33,27 @@ public class PlayerMovement : MonoBehaviour
     private void getInput(){
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        if(Input.GetKey(KeyCode.Space) && canFly == true)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up*15, ForceMode.Force); // maybe there should be VelocityChange
+            flyingTime -= Time.deltaTime;
+            if(flyingTime < 0){
+                canFly = false;
+                flyingTime = 0;
+            }
+        }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0,0,0); // lose all forces in order to fall down
+        }
+        else if(flyingTime < maxFlyingTime)
+        {
+            if(flyingTime > 0.3)
+                canFly = true;
+            flyingTime += Time.deltaTime * 0.1; // can be done using Mathf.Clamp()
+            if(flyingTime > maxFlyingTime)
+                flyingTime = maxFlyingTime;
+        }
     }
 
     private void Move(){
