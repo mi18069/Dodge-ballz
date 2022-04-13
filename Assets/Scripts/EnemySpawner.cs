1dbad047a2bf89a47b3b4ec2b2b90c60554d96d9
smajli99 <smajli9999@gmail.com>
2022-04-13 03:25:14 +0200

    Enemy spawn + gate opening

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private Vector3 spawnPoint;
+
+    [SerializeField] private List<GameObject> enemies;
+    [SerializeField] private int spawnTime;
+
+    [SerializeField] private int maxEnemy;
+    private int enemyCount;
+
+    private void Awake()
+    {
+        enemies = new List<GameObject>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator SpawnCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        while(true)
+        {        
+            GameObject instantiatedEnemy = Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+            enemies.Add(instantiatedEnemy);
+
+            enemyCount++;
+
+            if(enemyCount >= maxEnemy){
+                yield break;
+            }
+
+            yield return new WaitForSecondsRealtime(spawnTime);
+        }
+    }
+}
