b17213a555d42e07e7cdb26938f53b8d165f7241
NumberHunterP <numberhunterp@gmail.com>
2022-05-26 22:35:16 +0200

    bugfix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>(); 
+        isDestroyed = false;
     }
 
     // Update is called once per frame
